62a07225704d15a569a6020626f676e97128580e
Pablo <pablo.capistran123@gmail.com>
2022-09-19 22:39:53 -0500

    Cambios finales COMPLETO

--- ApiBiblio/Controllers/LibrosController.cs
+++ ApiBiblio/Controllers/LibrosController.cs
@@ -1,6 +1,46 @@
-﻿namespace ApiBiblio.Controllers
+﻿using ApiBiblio.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiBiblio.Controllers
 {
-    public class LibrosController
+    [ApiController]
+    [Route("libros")]
+    public class LibrosController : ControllerBase
     {
+        private readonly ApplicationDbContext dbContext;
+
+        public LibrosController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext; 
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Libro>>> Get()
+        {
+            return await dbContext.Libros.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Libro libros)
+        {
+            dbContext.Add(libros);
+            await dbContext.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(Libro libros, int id)
+        {
+            if (libros.Id != id)
+            {
+                return BadRequest("El id del libro no coincide con el establecido en la url.");
+            }
+
+            dbContext.Update(libros);
+            await dbContext.SaveChangesAsync();
+            return Ok();
+        }
     }
 }
---
+++ ApiBiblio/Migrations/20220919183925_Inicial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiBiblio.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Libros",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Aid = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Libros", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Libros");
+        }
+    }
+}
